882b27014988a09f6a871f4f0d51a223a5ce5247
Bruno Borges <brunobborges68@gmail.com>
2020-12-10 15:05:01 -0500

    adding placeholder js still waiting for api authentication

--- Assets/app.js
+++ Assets/app.js
@@ -1,7 +1,11 @@
-$.get( "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={c5fd94db03643d3ada48616c3ca850e5}", function( data ) {
+var city = document.getElementById('#search');
+var api = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&cnt=3&appid=c5fd94db03643d3ada48616c3ca850e5';
+
+
+
+$.get( api, function( data ) {
     $( ".result" ).html( data );
     alert( "Load was performed." );
    
   });
 
-  
